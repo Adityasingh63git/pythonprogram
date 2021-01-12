302e605eeeffb793236fc53b369bdf81d12ed7e2
Adityasingh63git <69346457+Adityasingh63git@users.noreply.github.com>
2021-01-13 02:03:02 +0530

    Add files via upload

---
+++ extension program.py
@@ -0,0 +1,5 @@
+print("enter the name of the file ")
+x="file"
+y=".py"
+if y == ".py" :
+    print("its a python program")
---
+++ simple.py
@@ -0,0 +1,5 @@
+print("area of a circle ")
+r=123
+area = (3.14 * (r*r ))
+print("area of a circle is :" + area) 
+print("area")
