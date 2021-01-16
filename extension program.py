4bd86ed747885fb4f24530fa6a1c5c6b432b52db
Adityasingh63git <69346457+Adityasingh63git@users.noreply.github.com>
2021-01-16 15:42:07 +0530

    Update extension program.py

--- extension program.py
+++ extension program.py
@@ -1,5 +1,9 @@
 print("enter the name of the file ")
-x="file"
-y=".py"
-if y == ".py" :
-    print("its a python program")
+x= input("enter the file name")
+y=input(".py")
+if y == ".py"
+  print("its a python program")
+if y==".js"
+  print("its a javascript")
+if y==".java":
+   print("its a java  program")
