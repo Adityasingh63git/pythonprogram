e60a66ad01142b6435c7cbe245451204bf461d56
Adityasingh63git <69346457+Adityasingh63git@users.noreply.github.com>
2021-01-27 00:39:09 +0530

    updated the changes
    now i add the input function in it

--- fibonacciseries.py
+++ fibonacciseries.py
@@ -1,14 +1,23 @@
-# A simple generator function
-def my_gen():
-    n = 1
-    print('This is printed first')
-    # Generator function contains yield statements
-    yield n
+# Program to display the Fibonacci sequence up to n-th term
 
-    n += 1
-    print('This is printed second')
-    yield n
+nterms = int(input("How many terms? "))
 
-    n += 1
-    print('This is printed at last')
-    yield n
+# first two terms
+n1, n2 = 0, 1
+count = 0
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
